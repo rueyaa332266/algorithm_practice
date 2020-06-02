6979354b969cb720cd7af3b5c811c4808a67791d
rueyaa332266 <rueyaa332266@gmail.com>
2020-06-03 08:34:37 +0900

    add binary search

---
+++ 08_binary_search.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+// return target index or -1(not found)
+func binary_search(in []int, target int) int {
+	left := 0
+	right := len(in) - 1
+	for left < right {
+		var mid int
+		mid = (right + left) / 2
+		// Check left and right if there is no value between them
+		if mid < 1 {
+			if in[left] == target {
+				return left
+			} else if in[right] == target {
+				return right
+			} else {
+				return -1
+			}
+		}
+
+		if in[mid] == target {
+			return mid
+		} else if in[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+	return -1
+}
+
+func main() {
+	list := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	fmt.Println(binary_search(list, 6))
+}
