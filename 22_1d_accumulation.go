875609320e8d83af75bc1897cb133b753cdfa0a4
rueyaa332266 <rueyaa332266@gmail.com>
2020-06-22 08:52:30 +0900

    add 1d accumulation

---
+++ 22_1d_accumulation.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+type segment struct {
+	left  int
+	right int
+}
+
+func accumulation1d(in []int, set []segment) []int {
+	for _, v := range set {
+		in[v.left] = in[v.left] + 1
+		in[v.right] = in[v.right] - 1
+		fmt.Println(in)
+	}
+	fmt.Println(in)
+	for i := 1; i <= (len(in) - 1); i++ {
+		in[i] = in[i] + in[i-1]
+	}
+	return in
+
+}
+
+func main() {
+	seg1 := segment{1, 2}
+	seg2 := segment{4, 7}
+	seg3 := segment{2, 6}
+	seg4 := segment{3, 5}
+	set := []segment{seg1, seg2, seg3, seg4}
+	list := make([]int, 8)
+	fmt.Println(accumulation1d(list, set))
+}
