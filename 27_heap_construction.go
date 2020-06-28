ece61b325eaf3778f55678d665d419e0958d9984
rueyaa332266 <rueyaa332266@gmail.com>
2020-06-29 08:54:05 +0900

    add heap constructio

--- 27_heap_construction.go
+++ 27_heap_construction.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+func heapConstructio(heap []int) []int {
+	length := len(heap)
+	start := parent(length - 1)
+	for i := start; i >= 0; i-- {
+		heap = downHeap(heap, i)
+	}
+	return heap
+}
+
 func downHeap(heap []int, index int) []int {
 	var largest int
 	last := len(heap) - 1
@@ -24,13 +33,22 @@ func downHeap(heap []int, index int) []int {
 	return heap
 }
 
+func parent(in int) int {
+	if in == 0 {
+		return -1
+	}
+	if in%2 == 0 {
+		return in/2 - 1
+	}
+	return in / 2
+}
+
 func child(in int) (int, int) {
 	return in*2 + 1, (in + 1) * 2
 }
 
 func main() {
-	heap := []int{20, 18, 11, 5, 7, 6, 9, 3, 2, 4, 2, 5}
-	heap[0] = 1
-	fmt.Println(downHeap(heap, 0))
+	heap := []int{5, 8, 2, 20, 1, 3, 9, 6, 22, 15, 8, 10}
+	fmt.Println(heapConstructio(heap))
 
 }
